14bc2bad6054843b5f195c6327a1bb98ff691e76
Bobur Niyozov <76684466+bobur2308@users.noreply.github.com>
2026-06-09 21:16:47 +0500

    add hotel model and controller

---
+++ HotelListingApi/Controllers/HotelsController.cs
@@ -0,0 +1,48 @@
+﻿using HotelListingApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListingApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HotelsController : ControllerBase
+{
+    private static List<Hotel> hotels = new List<Hotel>()
+    {
+        new  Hotel{ Id = 1, Name = "Hotel 1",Address = "Hotel 1" ,Rating = 5.5 },
+        new  Hotel{ Id = 2, Name = "Hotel 2",Address = "Hotel 2" ,Rating = 4.2 },
+    };
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Hotel> Get()
+    {
+        return Ok(hotels);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Hotel> Get(int id)
+    {
+        var hotel = hotels.FirstOrDefault(h => h.Id == id);
+        if (hotel == null) 
+            return NotFound();
+        return hotel;
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+        
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        
+    }
+}
