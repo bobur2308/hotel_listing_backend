cdf7585670f4df0e57dd46d91c39bcb7186fd2cd
b.niyozov <bobur.niyazov0200@gmail.com>
2026-07-21 11:33:06 +0500

    update HotelsController to use dtos for crud

--- HotelListingApi/Controllers/HotelsController.cs
+++ HotelListingApi/Controllers/HotelsController.cs
@@ -1,4 +1,5 @@
 ﻿using HotelListingApi.Data;
+using HotelListingApi.DTOs.Hotel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,35 +17,59 @@ public class HotelsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
+    public async Task<ActionResult<IEnumerable<GetHotelsDto>>> GetHotels()
     {
-        return await _context.Hotels.Include(h => h.Country).ToListAsync();
+        var hotels =  await _context.Hotels
+            .Select(x => new GetHotelsDto(x.Id, x.FullName,x.Address,x.Rating,x.CountryId))
+            .ToListAsync();
+        
+        return Ok(hotels);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Hotel>> GetHotel(int id)
+    public async Task<ActionResult<GetHotelDto>> GetHotel(int id)
     {
-        var hotel = await _context.Hotels.Include(h => h.Country).FirstOrDefaultAsync(x => x.Id == id);
+        var hotel = await _context.Hotels
+            .Where(x => x.Id == id)
+            .Include(h => h.Country)
+            .Select(x => new GetHotelDto(x.Id,x.FullName,x.Address,x.Rating,x.Country!.FullName))
+            .FirstOrDefaultAsync();
 
         if (hotel == null)
         {
             return NotFound();
         }
-        return hotel;
+        return Ok(hotel);
     }
 
     [HttpPost]
-    public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+    public async Task<ActionResult<Hotel>> PostHotel(CreateHotelDto hotelDto)
     {
+        var hotel = new Hotel()
+        {
+            FullName = hotelDto.FullName,
+            Address = hotelDto.Address,
+            Rating = hotelDto.Rating,
+            CountryId = hotelDto.CountryId
+        };
         await _context.Hotels.AddAsync(hotel);
         await _context.SaveChangesAsync();
         return CreatedAtAction("GetHotel", new { id = hotel.Id }, hotel);
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutHotel(int id, Hotel hotel)
+    public async Task<IActionResult> PutHotel(int id, UpdateHotelDto hotelDto)
     {
-        if (id != hotel.Id) return BadRequest();
+        if (id != hotelDto.Id) return BadRequest();
+        
+        var hotel = await _context.Hotels.FirstOrDefaultAsync(x => x.Id == id);
+        
+        if (hotel == null) return NotFound();
+        
+        hotel.FullName = hotelDto.FullName;
+        hotel.Address = hotelDto.Address;
+        hotel.Rating = hotelDto.Rating;
+        hotel.CountryId = hotelDto.CountryId;
         
         _context.Entry(hotel).State = EntityState.Modified;
         
