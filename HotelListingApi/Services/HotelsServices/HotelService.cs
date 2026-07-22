2b03b2b4111d99001502ec95bbc437f53606d2a0
b.niyozov <bobur.niyazov0200@gmail.com>
2026-07-22 16:24:02 +0500

    update HotelService

--- HotelListingApi/Services/HotelsServices/HotelService.cs
+++ HotelListingApi/Services/HotelsServices/HotelService.cs
@@ -1,10 +1,12 @@
-﻿using HotelListingApi.Data;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using HotelListingApi.Data;
 using HotelListingApi.DTOs.Hotel;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListingApi.Services.HotelsServices;
 
-public class HotelService(HotelListingDbContext context) : IHotelService
+public class HotelService(HotelListingDbContext context,IMapper mapper) : IHotelService
 {
     public async Task<IEnumerable<GetHotelsDto>> GetHotels()
     {
@@ -26,12 +28,7 @@ public class HotelService(HotelListingDbContext context) : IHotelService
         var hotel =  await context.Hotels
             .Where(x => x.Id == id)
             .Include(h => h.Country)
-            .Select(x => new GetHotelDto(
-                x.Id,
-                x.FullName,
-                x.Address,
-                x.Rating,
-                x.Country!.FullName))
+            .ProjectTo<GetHotelDto>(mapper.ConfigurationProvider)
             .FirstOrDefaultAsync(); 
         
         return hotel;
